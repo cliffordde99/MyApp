e49f973932a3300a43a6e8c1affc78998cc43791
cliffordde99 <69628711+cliffordde99@users.noreply.github.com>
2020-08-13 11:46:56 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+let car= {
+    color : "oynx black",
+    engineModel : "V8",
+    typeofcar : "Mclaren",
+}
+
+document.write("<p>" + car.color + "</p>");
+
+function myFunction2 (fname, age, future, hometown) {
+    return " Hello " + fname + " .You are " + age + " years old. You want to be a " + future + " when you are older " + hometown + " is where you are from "
+}
+
+let ab = myFunction2("Clifford", "21", "Billionaire", "Cape Coast");
+document.write (ab);
